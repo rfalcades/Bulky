c93da751903ca5e840884d1f4e86dd75adfcb4f0
Ricardo Falcades <ricardo.falcades@ab-inbev.com>
2024-11-13 18:20:04 -0300

    Category view / theme

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+        public IActionResult Index()
+        {
+            var categories = _db.Categories.ToList();
+
+            return View(categories);
+        }
+    }
+}
